76ac8eb98171860c248d5b63c6316466086a142a
ciairuzz <ciro.bologna@gmail.com>
2020-03-14 08:10:27 +0000

    read cwe as csv

--- dataloader/src/test/java/com/asimov/dataloader/DemoApplicationTests.java
+++ dataloader/src/test/java/com/asimov/dataloader/DemoApplicationTests.java
@@ -2,22 +2,31 @@ package com.asimov.dataloader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import com.asimov.dataloader.service.DataLoaderService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 
+import io.digitalstate.stix.custom.StixCustomObject;
+import io.digitalstate.stix.custom.objects.CustomObject;
 import io.digitalstate.stix.json.StixParserValidationException;
 import io.digitalstate.stix.json.StixParsers;
 import io.digitalstate.stix.sdo.objects.Vulnerability;
@@ -45,8 +54,9 @@ class DemoApplicationTests {
 		vulnerabilities.forEach(vulnerability -> {
 			Vulnerability parsedVulnerability = null;
 			try {
-				parsedVulnerability = (Vulnerability)StixParsers.parseObject((vulnerability.toJsonString()));
-			assertEquals(vulnerability.toJsonString(), parsedVulnerability.toJsonString(), "parsed vulnerability is different from actual one");
+				parsedVulnerability = (Vulnerability) StixParsers.parseObject((vulnerability.toJsonString()));
+				assertEquals(vulnerability.toJsonString(), parsedVulnerability.toJsonString(),
+						"parsed vulnerability is different from actual one");
 			} catch (StixParserValidationException | IOException e) {
 				e.printStackTrace();
 			}
@@ -55,13 +65,27 @@ class DemoApplicationTests {
 
 	@Test
 	void parseVulnerabilityFromJavaObject() throws StixParserValidationException, IOException {
-		//check it's a valid json
+		// check it's a valid json
 		JsonNode cveJSON = mapper.readTree(new ClassPathResource("cve.json").getInputStream());
 		Vulnerability vulnerability = dataloaderservice.parse(cveJSON.get("cve"));
 		String jsonString = vulnerability.toJsonString();
-		Vulnerability parsedVulnerability = (Vulnerability)StixParsers.parseObject(jsonString);
-		assertEquals(jsonString,parsedVulnerability.toJsonString());
+		Vulnerability parsedVulnerability = (Vulnerability) StixParsers.parseObject(jsonString);
+		assertEquals(jsonString, parsedVulnerability.toJsonString());
 	}
 
-	
+	@Test
+	void readWeaknessFromCSV() throws IOException {
+		File csvFile = new ClassPathResource("cwe1000_researchConcepts.csv").getFile();
+		// File csvFile = new ClassPathResource("cwe.csv").getFile();
+		CsvMapper csvMapper = new CsvMapper();
+		CsvSchema schema = CsvSchema.emptySchema().withHeader(); // use first row as header; otherwise defaults are fine
+		MappingIterator<Map<String,String>> it = csvMapper.readerFor(Map.class)
+		   .with(schema)
+		   .readValues(csvFile);
+		while (it.hasNext()) {
+		  Map<String,String> rowAsMap = it.next();
+		  // access by column name, as defined in the header row...
+		  System.out.println(mapper.writeValueAsString(rowAsMap));
+		}
+	}
 }
